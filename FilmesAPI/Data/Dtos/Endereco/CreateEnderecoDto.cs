1fe054fb21026c69226b5d7460132b14c39a2f54
João Sátiro <joao.v.satiro@gmail.com>
2021-09-29 22:41:41 -0300

    add endereco

---
+++ FilmesAPI/Data/Dtos/Endereco/CreateEnderecoDto.cs
@@ -0,0 +1,15 @@
+﻿using FilmesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+  public class CreateEnderecoDto
+  {
+    public string Logradouro { get; set; }
+    public string Bairro { get; set; }
+    public int Numero { get; set; }
+  }
+}
---
+++ FilmesAPI/Data/Dtos/Endereco/UpdateEnderecoDto.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+  public class UpdateEnderecoDto
+  {
+    public string Logradouro { get; set; }
+
+    public string Bairro { get; set; }
+
+    public int Numero { get; set; }
+  }
+}
